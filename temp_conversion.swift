4998f1743006f5d4ec1b13780a261008ec920845
Stephanholly <sholly@usc.edu>
2016-10-19 11:40:22 -0700

    challenge 4

---
+++ temp_conversion.swift
@@ -0,0 +1,9 @@
+
+// Write your code below! NOTE: you may need to look up the formula for temperature conversion. Remember: Google is your friend!
+
+
+// Create a method, called CtoF, that converts temperature in Celsius to Fahrenheit
+
+
+
+// Create a method, called FtoC, that converts temperature in Fahrenheit to Celsius
